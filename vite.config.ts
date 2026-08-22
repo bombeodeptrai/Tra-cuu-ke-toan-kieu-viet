4a4ca153b2638ec4535e353b1a4ab9b5afc96396
bombeodeptrai <nguyenthanhtrongnhan14@gmail.com>
2026-08-22 17:00:58 +0700

    Cập nhật đường dẫn base url theo tên miền Tra-cuu-ke-toan-kieu-viet

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Tra-cuu-ke-toan-kieu-vi-t/',
+  base: '/Tra-cuu-ke-toan-kieu-viet/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
