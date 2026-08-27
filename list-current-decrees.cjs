a5fe6b184ba2ed40ad3f6a2c9a27503b5cb4601c
bombeodeptrai <nguyenthanhtrongnhan14@gmail.com>
2026-08-27 17:27:19 +0700

    chore: clean up generation scripts

--- list-current-decrees.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const decrees = JSON.parse(fs.readFileSync('public/data/decrees.json', 'utf8'));
-console.log('Current decrees in DB:');
-decrees.forEach((d, idx) => {
-  console.log(`${(idx+1).toString().padStart(2, '0')}. [${d.category}] ${d.decree_number} - ${d.title} (Ban hành: ${d.issued_date})`);
-});
